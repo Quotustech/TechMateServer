0051fe85715b5ef4341c80db466e2ca879b8b7eb
Abiksoumya <soumyamohanty540@gmail.com>
2023-09-22 11:02:21 +0530

    clear logs

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -15,11 +15,9 @@ const Register = async (req, res) => {
             return res.status(400).json({ message: 'Username already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        console.log("auth token", hashedPassword)
 
         const newUser = new User({ name, email: email, password: hashedPassword });
         const savedUser = await newUser.save();
-        console.log(res)
         return res.status(201).json({ savedUser, message: 'User registered successfully' });
     }
     catch (error) {
@@ -36,20 +34,17 @@ const Login = async (req, res) => {
         // Find the user
         const users = await User.find()
         const user = users.find(u => u.email === email);
-        console.log("loged data", user)
         // check the user
         if (!user) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
         const passwordMatch = await bcrypt.compare(password, user.password);
-        console.log("password", passwordMatch)
         if (!passwordMatch) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         // Generate a JWT token
         const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
-        console.log(res)
         return res.status(200).json({ token, message: 'User login successfully' });
 
     }
